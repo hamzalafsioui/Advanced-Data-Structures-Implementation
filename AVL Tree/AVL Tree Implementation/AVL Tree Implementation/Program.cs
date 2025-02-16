3355172cac9cf35b809e1239288ed49afc062aaf
hamzalafsioui <hamzalafsioui@gmail.com>
2025-02-16 13:53:23 +0000

    AVL Tree

---
+++ AVL Tree/AVL Tree Implementation/AVL Tree Implementation/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace AVL_Tree_Implementation;
+
+internal class Program
+{
+
+	class AVLNode
+	{
+		public int Value { get; set; }
+		public AVLNode Right { get; set; }
+		public AVLNode Left { get; set; }
+		public int Height { get; set; }
+		public AVLNode(int value)
+		{
+			Value = value;
+			Height = 1;
+		}
+	}
+	class AVLTree
+	{
+		private AVLNode root;
+
+		public void Insert(int value)
+		{
+			root = Insert(root, value);
+		}
+
+		private AVLNode Insert(AVLNode node, int value)
+		{
+			if (node == null)
+				return new AVLNode(value);
+
+			if (value < node.Value)
+				node.Left = Insert(node.Right, value);
+			else if (value > node.Value)
+				node.Right = Insert(node.Right, value);
+			else
+				return node;
+
+			UpdateHeight(node);
+		}
+
+		private void UpdateHeight(AVLNode root)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+
+
+	static void Main(string[] args)
+	{
+
+	}
+}
