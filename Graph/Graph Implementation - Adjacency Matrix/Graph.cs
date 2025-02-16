1c4a8b51eca433c72f403d752838e67f6f12d62d
hamzalafsioui <hamzalafsioui@gmail.com>
2025-02-16 20:46:31 +0000

    Graph Implementation - Adjacency_Matrix

---
+++ Graph/Graph Implementation - Adjacency Matrix/Graph.cs
@@ -0,0 +1,169 @@
+﻿namespace Graph_Implementation___Adjacency_Matrix
+{
+	public class Graph
+	{
+
+		public enum enGraphDirectionType
+		{
+			Directed,
+			Undirected
+		}
+
+		// Adjacency matrix to store the weights of edges between vertices
+		private int[,] _adjacencyMatrix;
+
+		// Dictionary to map string vertices to integer indices for adjacency matrix
+		private Dictionary<string, int> _vertexDictionary;
+		// Number of vertices in the graph
+		private int _numberOfVertices;
+		private enGraphDirectionType _graphDirectionType = enGraphDirectionType.Undirected;
+
+		// Constructor: Initializes the adjacency matrix and the vertex mapping
+		public Graph(List<string> vertices, enGraphDirectionType enGraphDirectionType)
+		{
+			_graphDirectionType = enGraphDirectionType;
+			// Set the number of vertices
+			_numberOfVertices = vertices.Count;
+			// Initialize a 2D array (matrix) of size vertices x vertices
+			_adjacencyMatrix = new int[_numberOfVertices, _numberOfVertices];
+			// Initialize the dictionary to map vertex names to matrix indices
+			_vertexDictionary = new Dictionary<string, int>();
+
+			// Populate the dictionary with vertices (e.g., 'A' -> 0, 'B' -> 1, etc.)
+			for (int i = 0; i < _numberOfVertices; i++)
+			{
+				_vertexDictionary[vertices[i]] = i;
+			}
+		}
+
+		// Method to add a weighted edge between two vertices (source and destination)
+		public void AddEdge(string source, string destination, int weight)
+		{
+			// Check if both source and destination vertices exist in the map
+			if (_vertexDictionary.ContainsKey(source) && _vertexDictionary.ContainsKey(destination))
+			{
+				// Retrieve the indices for the source and destination vertices
+				int sourceIndex = _vertexDictionary[source];
+				int destinationIndex = _vertexDictionary[destination];
+				// Set the matrix value to the weight for  [sourceIndex, destinationIndex] : one way
+				_adjacencyMatrix[sourceIndex, destinationIndex] = weight;
+				//Set the matrix value to the weight for  [destinationIndex,sourceIndex ] : Two ways incase of undirected graph
+				if (_graphDirectionType == enGraphDirectionType.Undirected)
+				{
+					_adjacencyMatrix[destinationIndex, sourceIndex] = weight;
+				}
+			}
+			else
+			{
+				// If either vertex is invalid, show an error message
+				Console.WriteLine($"\n\nIgnored: Invalid vertices. {source} ==> {destination}");
+			}
+		}
+
+		// Method to remove an edge between two vertices
+		public void RemoveEdge(string source, string destination)
+		{
+			// Check if both source and destination vertices exist in the map
+			if (_vertexDictionary.ContainsKey(source) && _vertexDictionary.ContainsKey(destination))
+			{
+				// Retrieve the indices for the source and destination vertices
+				int sourceIndex = _vertexDictionary[source];
+				int destinationIndex = _vertexDictionary[destination];
+				// Set the matrix value to 0 for [sourceIndex, destinationIndex] : one way
+				_adjacencyMatrix[sourceIndex, destinationIndex] = 0;
+				// Set the matrix value to 0 for [destinationIndex, sourceIndex] : Two ways incase of undirected graph
+				if (_graphDirectionType == enGraphDirectionType.Undirected)
+				{
+					_adjacencyMatrix[destinationIndex, sourceIndex] = 0;
+				}
+			}
+			else
+			{
+				// If either vertex is invalid, show an error message
+				Console.WriteLine($"\n\nIgnored: Invalid vertices. {source} ==> {destination}");
+			}
+		}
+
+		// Method to display the adjacency matrix (graph representation) with string labels
+		public void DisplayGraph(string message, int space = 4)
+		{
+			Console.WriteLine($"\n\n{message}");
+			// Print the header row (vertex labels)
+			Console.Write("".PadRight(space));
+			foreach (var vertex in _vertexDictionary)
+			{
+				Console.Write($"{vertex.Key}".PadRight(space));
+			}
+			Console.WriteLine();
+			// Print the matrix rows
+			for (int i = 0; i < _numberOfVertices; i++)
+			{
+				// Print the vertex label for each row
+				Console.Write($"{_vertexDictionary.ElementAt(i).Key}".PadRight(space));
+				// Print the values for each row
+				for (int j = 0; j < _numberOfVertices; j++)
+				{
+					// Print the value at _adjacencyMatrix[source.Value, j]
+					Console.Write($"{_adjacencyMatrix[i, j]}".PadRight(space));
+				}
+				Console.WriteLine();
+			}
+		}
+
+		// Method to check if there's an edge between two vertices
+		public bool IsEdge(string source, string destination)
+		{
+			// Check if both source and destination vertices exist in the map
+			if (_vertexDictionary.ContainsKey(source) && _vertexDictionary.ContainsKey(destination))
+			{
+				int sourceIndex = _vertexDictionary[source];
+				int destinationIndex = _vertexDictionary[destination];
+				//if weight >0 this means there is an edge between two verticies.
+				return _adjacencyMatrix[sourceIndex, destinationIndex] > 0;
+			}
+			return false;
+		}
+
+		// Method to get the Indegree of a vertex (i.e., how many edges are connected to it (can reach it))
+		public int GetInDegree(string vertex)
+		{
+			var degree = 0;// Initialize the degree count to zero
+						   // Check if the vertex exists in the map
+			if (_vertexDictionary.ContainsKey(vertex))
+			{
+				int vertexIndex = _vertexDictionary[vertex];
+				// Loop through all possible connections (columns) for the given vertex (row)
+				for (int i = 0; i < _numberOfVertices; i++)
+				{
+					if (_adjacencyMatrix[i, vertexIndex] > 0)
+					{
+						// If there's an edge (i.e., weight is greater than 0), increment the degree count
+						degree++;
+					}
+				}
+			}
+			return degree;
+		}
+
+		// Method to get the Outdegree of a vertex (i.e., how many edges out of it and can go to)
+		public int GetOutDegree(string vertex)
+		{
+			// Initialize the degree count to zero
+			var degree = 0;
+			// Loop through all possible connections (columns) for the given vertex (row)
+			if (_vertexDictionary.ContainsKey(vertex))
+			{
+				int vertexIndex = _vertexDictionary[vertex];
+				for (int i = 0; i < _numberOfVertices; i++)
+				{
+					// If there's an edge (i.e., weight is greater than 0), increment the degree count
+					if (_adjacencyMatrix[vertexIndex, i] > 0)
+					{
+						degree++;
+					}
+				}
+			}
+			return degree;
+		}
+	}
+}
---
+++ Graph/Graph Implementation - Adjacency Matrix/Program.cs
@@ -0,0 +1,109 @@
+﻿using static Graph_Implementation___Adjacency_Matrix.Graph;
+
+namespace Graph_Implementation___Adjacency_Matrix
+{
+	public class Program
+	{
+
+		static void Main(string[] args)
+		{
+			// Create a list of vertices with string labels
+			List<string> vertices = new List<string> { "A", "B", "C", "D", "E" };
+
+
+			// Create a new graph object with the specified vertices
+			Graph graph1 = new Graph(vertices, enGraphDirectionType.Undirected);
+
+			// Add some edges with default weights=1 between vertices
+			graph1.AddEdge("A", "B", 1);
+			graph1.AddEdge("A", "C", 1);
+			graph1.AddEdge("B", "D", 1);
+			graph1.AddEdge("C", "D", 1);
+			graph1.AddEdge("B", "E", 1);
+			graph1.AddEdge("D", "E", 1);
+
+
+
+			// Display the adjacency matrix with weights to visualize the graph
+			graph1.DisplayGraph("Adjacency Matrix for Example1 (Undirected Graph):");
+
+			Console.WriteLine("\n------------------------------\n");
+
+			// Create a new graph object with the specified vertices
+			Graph graph2 = new Graph(vertices, enGraphDirectionType.Directed);
+
+			// Add some edges with weights between vertices
+			graph2.AddEdge("A", "A", 1);
+			graph2.AddEdge("A", "B", 1);
+			graph2.AddEdge("A", "C", 1);
+			graph2.AddEdge("B", "E", 1);
+			graph2.AddEdge("D", "B", 1);
+			graph2.AddEdge("D", "C", 1);
+			graph2.AddEdge("D", "E", 1);
+
+			// Display the adjacency matrix with weights to visualize the graph
+			graph2.DisplayGraph("Adjacency Matrix for Example2 (Directed Graph):");
+
+
+			// Get and display the Indegree (number of edges) of vertex 'D'
+			Console.WriteLine("\nInDegree of vertex D: " + graph2.GetInDegree("D"));
+
+			// Get and display the Outdegree (number of edges) of vertex 'D'
+			Console.WriteLine("\nOutDegree of vertex D: " + graph2.GetOutDegree("D"));
+
+			Console.WriteLine("\n------------------------------\n");
+
+			// Create a new graph object with the specified vertices
+			Graph graph3 = new Graph(vertices, enGraphDirectionType.Undirected);
+
+			// Add some edges with weights between vertices
+			graph3.AddEdge("A", "B", 5);
+			graph3.AddEdge("A", "C", 3);
+			graph3.AddEdge("B", "D", 12);
+			graph3.AddEdge("C", "D", 10);
+			graph3.AddEdge("B", "E", 2);
+			graph3.AddEdge("D", "E", 7);
+
+			// Display the adjacency matrix with weights to visualize the graph
+			graph3.DisplayGraph("Adjacency Matrix for Example3 (Weighted Graph):");
+
+
+			// Check if there is an edge between 'A' and 'B' and display the result
+			Console.WriteLine("\nIs there an edge between A and B in Graph3? " + graph3.IsEdge("A", "B"));
+
+
+			// Check if there is an edge between 'A' and 'B' and display the result
+			Console.WriteLine("\nIs there an edge between B and C in Graph3? " + graph3.IsEdge("B", "C"));
+
+
+			// Check if there is an edge between 'A' and 'A' and display the result
+			Console.WriteLine("\nIs there an edge between E and D in Graph3? " + graph3.IsEdge("E", "D"));
+
+			// Check if there is an edge between 'A' and 'A' and display the result
+			Console.WriteLine("\nIs there an edge between A and A in Graph3? " + graph3.IsEdge("A", "A"));
+
+
+			// Get and display the Indegree (number of edges) of vertex 'A'
+			Console.WriteLine("\nInDegree of vertex A: " + graph3.GetInDegree("A"));
+
+			// Get and display the Indegree (number of edges) of vertex 'A'
+			Console.WriteLine("\nOutDegree of vertex A: " + graph3.GetOutDegree("A"));
+
+			Console.WriteLine("\n------------------------------\n");
+
+
+			Console.WriteLine("\nRemoveing Edge between E and D:");
+			// Remove the edge between 'E' and 'D'
+			graph3.RemoveEdge("E", "D");
+
+			// Display the updated adjacency matrix after removing the edge
+			graph3.DisplayGraph("After Removeing Edge between E and D:");
+
+
+			// Check if there is an edge between 'A' and 'A' and display the result
+			Console.WriteLine("\nIs there an edge between E and D in Graph3? " + graph3.IsEdge("E", "D"));
+
+
+		}
+	}
+}
